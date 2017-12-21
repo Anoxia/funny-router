6cb7f3647f8c6015572647afe1e1f31ec2161d1d
路易港 <anoxia@engineer.com>
2017-12-21 21:44:16 +0800

    项目名迁移

---
+++ src/Router.php
@@ -0,0 +1,18 @@
+<?php
+namespace Funny;
+
+/**
+ * Router
+ * @package Funny
+ */
+class Router
+{
+    /**
+     * @param string $url
+     * @param string $method
+     */
+    public function handle($url, $method)
+    {
+
+    }
+}
